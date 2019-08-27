0f77e997a2ad1b57663bfea7aae0aa142ab50be1
Eric <eric_tj@126.com>
2019-08-27 22:35:35 +0800

    Implement mount/umount command

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const fs = require('fs')
 const {exec} = require('child_process')
+const req = require('electron').remote.require;
+const { execSync } = req('child_process');
 
 let app_data = new Vue ({
   el: 'div.content',
   data: {
-    folders: []
+    folders: [],
+    password: ''
   }
 })
 
@@ -12,7 +15,9 @@ function read_config(cb) {
   fs.readFile('./config.json', (err, data) => {
     if (err)
       return cb(err);
-    app_data.folders = JSON.parse(data);
+    let config_data = JSON.parse(data)
+    app_data.folders = config_data['folders'];
+    app_data.password = config_data['password'];
     return cb()
   })
 }
@@ -32,6 +37,33 @@ function refresh_mount_state()
 
 function h_mount_folder(folder)
 {
+  let cmd = 'sudo -S mount';
+  if (folder['options'] != null) {
+    for (opt of folder['options']) {
+      cmd += " -o " + opt
+    }
+  }
+
+  cmd += ' ' + folder['src'] + ' ' + folder['dest'];
+  console.log(cmd);
+  try {
+    execSync(cmd, {input: app_data.password});
+    folder.state = 'mounted'
+  } catch (e) {
+    console.log('mount exception: ' +e)
+  }
+}
+
+function h_umount_folder(folder)
+{
+  console.log("umount " + folder['dest']);
+  let cmd = 'sudo -S umount -l -f ' + folder['dest']
+  try {
+    execSync(cmd, {input: app_data.password});
+    folder.state = 'unmounted'
+  } catch (e) {
+    console.log('umount exception: ' +e)
+  }
 }
 
 $(document).ready(() => {
